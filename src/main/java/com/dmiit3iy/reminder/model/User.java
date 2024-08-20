613e4ccde127b6228ed7854202b8cd9bb73a384a
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2024-08-20 11:44:20 +0500

    add get and add method in User for List<Rem..>

--- src/main/java/com/dmiit3iy/reminder/model/User.java
+++ src/main/java/com/dmiit3iy/reminder/model/User.java
@@ -1,15 +1,18 @@
 package com.dmiit3iy.reminder.model;
 
 import lombok.*;
+import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +23,15 @@ public class User {
     private String lastName;
     private String email;
     private String telegram;
+    @OneToMany(mappedBy = "user")
+    @Cascade(value = org.hibernate.annotations.CascadeType.DELETE)
+    private List<Reminder> reminders = new ArrayList<>();
+
+    public void addRemind(Reminder reminder) {
+        reminders.add(reminder);
+    }
+
+    public List<Reminder> getReminds() {
+        return reminders;
+    }
 }
