33fa8ceb5aef66654c5f3113b5b9f2aced850b31
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2024-08-22 09:29:28 +0500

    add DTO object

---
+++ src/main/java/com/dmiit3iy/reminder/DTO/ResponseResult.java
@@ -0,0 +1,15 @@
+package com.dmiit3iy.reminder.DTO;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseResult<T> {
+    private String message;
+    private T data;
+}
