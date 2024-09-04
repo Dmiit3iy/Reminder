9ff48af083fda60eea542f258de61785abed1459
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2024-09-04 14:22:59 +0500

    add get controller whith pagination

--- src/main/java/com/dmiit3iy/reminder/controller/ReminderController.java
+++ src/main/java/com/dmiit3iy/reminder/controller/ReminderController.java
@@ -4,6 +4,8 @@ import com.dmiit3iy.reminder.DTO.ResponseResult;
 import com.dmiit3iy.reminder.model.Reminder;
 import com.dmiit3iy.reminder.service.ReminderService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,39 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class ReminderController {
     private final ReminderService reminderService;
+
+    /**
+     * Получение всего списка напоминаний
+     *
+     * @param pageable
+     * @return
+     */
+    @GetMapping("/list")
+    public ResponseEntity<ResponseResult<Page<Reminder>>> getUsers(Pageable pageable) {
+        try {
+            Page<Reminder> page = reminderService.get(pageable);
+            return new ResponseEntity<>(new ResponseResult<>(null, page), HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(new ResponseResult<>(e.getMessage(), null), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Получение списка с пагинацией, с параметрами
+     *
+     * @param page
+     * @param size
+     * @return
+     */
+    @GetMapping("/list")
+    public ResponseEntity<ResponseResult<Page<Reminder>>> getUsers(@RequestParam("page") int page, @RequestParam("size") int size) {
+        try {
+            Page<Reminder> pageRemind = reminderService.get(page, size);
+            return new ResponseEntity<>(new ResponseResult<>(null, pageRemind), HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(new ResponseResult<>(e.getMessage(), null), HttpStatus.BAD_REQUEST);
+        }
+    }
 
     /**
      * Создание напоминания
