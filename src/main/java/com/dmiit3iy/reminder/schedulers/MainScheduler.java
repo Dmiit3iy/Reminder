ecd6f9ecce4c62a9383d1c01aba729963bb705bd
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2024-09-19 14:09:32 +0500

    make send multithreads

--- src/main/java/com/dmiit3iy/reminder/schedulers/MainScheduler.java
+++ src/main/java/com/dmiit3iy/reminder/schedulers/MainScheduler.java
@@ -3,12 +3,12 @@ package com.dmiit3iy.reminder.schedulers;
 import com.dmiit3iy.reminder.model.Reminder;
 import com.dmiit3iy.reminder.service.EmailService;
 import com.dmiit3iy.reminder.service.ReminderService;
-import com.dmiit3iy.reminder.service.UserService;
 import com.dmiit3iy.reminder.telegram.service.TelegramService;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import static com.pengrad.telegrambot.model.request.ParseMode.HTML;
@@ -16,27 +16,41 @@ import static com.pengrad.telegrambot.model.request.ParseMode.HTML;
 @AllArgsConstructor
 @Component
 public class MainScheduler {
-    private final UserService userService;
+
     private final ReminderService reminderService;
     private final TelegramService telegramService;
     private final EmailService emailService;
-    //private LinkedBlockingQueue<Reminder> linkedBlockingQueue = new LinkedBlockingQueue<>();
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
 
     @Scheduled(cron = "0 * * * * *", zone = "Europe/Moscow")
     public void sendCompliment() {
         System.out.println("Начинаем рассылку!");
         LinkedBlockingQueue<Reminder> linkedBlockingQueue = new LinkedBlockingQueue<>(reminderService.getToSend());
-        while (linkedBlockingQueue.size() > 0) {
+        while (!linkedBlockingQueue.isEmpty()) {
             try {
-                Reminder x = linkedBlockingQueue.take();
-                emailService.sendMessage(x.getUser().getEmail(), x.getTitle() + "\n" + x.getDescription());
-                telegramService.sendMessage(x.getUser().getChatId(), x.getTitle() + "\n" + x.getDescription(), HTML);
-                x.setSend(true);
-                reminderService.update(x);
+                Reminder reminder = linkedBlockingQueue.take();
+                threadPool.submit(() -> {
+                    try {
+                        emailService.sendMessage(reminder.getUser().getEmail(), reminder.getTitle() + "\n" + reminder.getDescription());
+                        telegramService.sendMessage(reminder.getUser().getChatId(), reminder.getTitle() + "\n" + reminder.getDescription(), HTML);
+                        reminder.setSend(true);
+                        reminderService.update(reminder);
+                    } catch (Exception e) {
+                        System.err.println("Ошибка при отправке напоминания: " + reminder.getTitle() + " id:" + reminder.getId());
+                        e.printStackTrace();
+                    }
+                });
             } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Ошибка при получении напоминания из очереди");
                 throw new RuntimeException(e);
             }
         }
     }
+
+    public void shutdownThreadPool() {
+        threadPool.shutdown();
+    }
+
 }
